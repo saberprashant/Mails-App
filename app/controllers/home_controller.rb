fa9288058f5a50681d8500eec078d8b04453e347
Prashant Jain <prashantjain.pro@gmail.com>
2020-01-26 23:07:05 +0530

    cleaning

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,6 @@
 class HomeController < ApplicationController
   def index
     # @notice = 'Welcome to Mails App'
-    # @todo - UI work on index page and in index.html.erb for home view
-
-    
     # redirect_to mails_path
   end
 end
